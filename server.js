abab27bb146d7163da6578e8c7ded49206d84c80
Keoki <keoki.alan.mundon@gmail.com>
2022-03-17 18:29:31 -0700

    sending index and show data

---
+++ server.js
@@ -0,0 +1,24 @@
+// DEPENDENCIES
+const express = require('express')
+// const mongoose = require('mongoose')
+
+// CONFIGURATION
+require('dotenv').config()
+const PORT = process.env.PORT
+const app = express()
+
+// ROUTES
+app.get('/', (req, res) => {
+    res.send('Welcome to an Awesome App about Breads!')
+})
+
+// Breads
+const breadsController = require('./controllers/breads_controller.js')
+app.use('/breads', breadsController)
+
+// LISTEN
+app.listen(PORT, () => {
+    console.log('nomming at port', PORT)
+})
+
+// mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
