abb86012e385f89044c1abd0cb757075971af0c9
Keoki <keoki.alan.mundon@gmail.com>
2022-03-20 09:54:13 -0700

    sending index and show data

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT
 const app = express()
 
 // MIDDLEWARE
+app.use(express.static('public'))
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
@@ -21,9 +22,13 @@ app.get('/', (req, res) => {
 const breadsController = require('./controllers/breads_controller.js')
 app.use('/breads', breadsController)
 
+// 404 Page
+app.get('*', (req, res) => {
+    res.send('404')
+  })
+
 // LISTEN
 app.listen(PORT, () => {
     console.log('nomming at port', PORT)
 })
 
-// mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
